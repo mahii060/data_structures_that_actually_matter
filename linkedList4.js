5d0512233aedaa1e3db6f4fdd427f00e6f3c00c4
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2025-11-11 20:57:16 +0600

    Implemented linked list

--- linkedList4.js
+++ linkedList4.js
@@ -17,7 +17,7 @@ class LinkedList {
         const newNode = new Node(value);
         if (this.head === null) {
             this.head = newNode;
-            this.tail = newNode;
+            this.tail = this.head;
         } else {
             this.tail.next = newNode; // as i'm appending so tail.next moved to newNode.
             this.tail = newNode
@@ -60,26 +60,92 @@ class LinkedList {
         if (index === this.length) {
             return this.append(value)
         }
+
+        // Add node in the middle of the linked list
+        const leadingNode = this._traverseToIndex(index);
+        const holdingNode = leadingNode.next;
+
+        const newNode = new Node(value);
+        leadingNode.next = newNode;
+        newNode.next = holdingNode;
+
+        this.length++;
+
+        return this;
+
     }
 
     // remove node from any index
-    remove() { }
+    remove(index) {
+        if (index < 0 || index >= this.length) {
+            console.error("Sorry, Can't remove node, Index is out of bound!")
+            return undefined
+        }
+
+        if (index === 0) {
+            this.head = this.head.next;
+            // If there is only one node in the linked list.
+            if (this.length === 1) {
+                this.tail = null;
+            }
+            this.length--;
+
+            return this;
+        }
+
+        // remove from any index or tail
+        const leadingNode = this._traverseToIndex(index - 1);
+        const nodeToRemove = leadingNode.next;
+
+        leadingNode.next = nodeToRemove.next;
+
+        // If i remove the last node
+        if (leadingNode.next === null) {
+            this.tail = leadingNode;
+        }
+
+        this.length--;
+        return this
+
+    }
+
+    // private function
+    _traverseToIndex(index) {
+        let count = 0;
+        let currentNode = this.head;
+        while (count !== index - 1) {
+            currentNode = currentNode.next;
+            count++;
+        }
+    }
 
     // Check if the linked list is empty. return true or false
     isEmpty() { }
 
     // Print the whole linked list
-    print() { }
+    print() {
+        const values = [];
+        let current = this.head;
+        while (current) {
+            values.push(current.value);
+            current = current.next;
+        }
+        console.log(values);
+    }
 }
 
 const newLinkedList = new LinkedList();
 
-newLinkedList.append(1)
-newLinkedList.append(2)
-newLinkedList.append(3)
+newLinkedList.append(1) // index 5
+newLinkedList.append(2) // index 4
+newLinkedList.append(3) // index 3
 
-newLinkedList.prepend(33)
-newLinkedList.prepend(22)
-newLinkedList.prepend(11)
+// newLinkedList.remove(3)
 
-console.log(newLinkedList);
+newLinkedList.prepend(33) // index 2
+newLinkedList.prepend(22) // index 1
+newLinkedList.prepend(11) // index 0
+
+newLinkedList.print()
+
+// console.log(newLinkedList);
